ded5b1d34e34f53784a64e6659568922790f52b8
markcalendario <markcalendario@gmail.com>
2024-04-30 23:37:34 +0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,201 @@
+const locations = {
+  "interfaith-chapel": {
+    title: "Interfaith Chapel",
+    imageName: "interfaith-chapel.jpg",
+    description:
+      "The Interfaith Chapel is a sacred space designed to provide a welcoming environment for people of all faiths and beliefs to come together for prayer, reflection, and meditation, fostering spiritual growth, understanding, and respect for diversity."
+  },
+
+  "north-wing": {
+    title: "North Wing",
+    imageName: "north-wing.jpg",
+    description:
+      "One of the wing of North, East, West, and South Wing Building of the Polytechnic University of the Philippines."
+  },
+
+  pool: {
+    title: "Olympic Pool",
+    imageName: "pool.jpg",
+    description:
+      "An olympic-sized swimming pool that is capable of handling several swimming classes and student athlete activities simultaneously."
+  },
+
+  lagoon: {
+    title: "Lagoon",
+    imageName: "lagoon.jpg",
+    description:
+      "The PUP Lagoon has new landscapes and hardscapes, and renovated stalls. The new lagoon will also serve as a park and open space for students."
+  },
+
+  obelisk: {
+    title: "Obelisk",
+    imageName: "obelisk.jpg",
+    description:
+      "The Obelisk, standing majestic on its base, depicts the strength of the Polytechnic University of the Philippines as an institution of higher learning, promoting educational and moral aims which are fortified by a determined leadership with a clear vision for the Filipino youth and an efficient support system inspired by the virtues of public service."
+  },
+
+  pylon: {
+    title: "Pylon",
+    imageName: "pylon.jpg",
+    description:
+      "The Triad of pillars may also stand for wisdom, strength and beauty because there should be wisdom to contrive, strength to support and beauty to adorn any great or important undertaking."
+  },
+
+  shrine: {
+    title: "Mabini Shrine",
+    imageName: "shrine.jpg",
+    description:
+      "The Triad of pillars may also stand for wisdom, strength and beauty because there should be wisdom to contrive, strength to support and beauty to adorn any great or important undertaking."
+  },
+
+  "ninoy-library": {
+    title: "Ninoy Aquino Library",
+    imageName: "ninoy-library.jpg",
+    description:
+      "Ninoy Aquino Library and Learning Resources Center of the Polytechnic University of the Philippines, Santa Mesa, Manila, the Philippines"
+  },
+
+  "water-tank": {
+    title: "R.C. Overhead Water Tank",
+    imageName: "water-tank.jpg",
+    description:
+      "The R.C. Water Tank located beside the Ninoy Aquino Library and Learning Resources Center. This was coined as PUP space ship."
+  },
+
+  "laboratory-highschool": {
+    title: "Laboratory High School",
+    imageName: "laboratory-highschool.jpg",
+    description:
+      "The PUP Laboratory High School (PUP LHS) was established in 1954 during the Philippine College of Commerce era. The PUP LHS curricular program focuses on Discipline, Academic Excellence, Nationalism and Commerce. It is one of the top performing high schools in the country producing top caliber students who later become prominent in their chosen field of specialization."
+  },
+
+  "charlie-building": {
+    title: "Charlie Del Rosario Building",
+    imageName: "charlie-building.jpg",
+    description:
+      "A facility in the Polytechnic University of the Philippines named to Charlie del Rosario, a university professor and activist who was the first person reported missing during the Martial Law regime on March 19, 1971"
+  },
+
+  gymnasium: {
+    title: "Gymnasium and Sports Center",
+    imageName: "gymnasium.jpg",
+    description:
+      "The University Gymnasium serves as the training ground for the student athletes that participate in the intercollegiate sports program."
+  },
+
+  "tennis-court": {
+    title: "Tennis Court",
+    imageName: "tennis-court.jpg",
+    description:
+      "Outdoor gym fit for tennis games. Located beside the University Gymnasium."
+  },
+
+  "basketball-court": {
+    title: "Basketball and Volleyball Court",
+    imageName: "basketball-court.jpg",
+    description:
+      "Outdoor gym fit for basketball and volleyball games. Located beside the University Gymnasium."
+  },
+
+  oval: {
+    title: "Track and Football Oval",
+    imageName: "oval.jpg",
+    description:
+      "A football field that can serve as softball, soccer, and football fields for students."
+  },
+
+  grandstand: {
+    title: "Grandstand",
+    imageName: "grandstand.jpg",
+    description:
+      "The grandstand is a roofed seating area in the University's Oval, commanding the best view for spectators at events happening in the oval."
+  },
+
+  amphitheather: {
+    title: "Amphitheather",
+    imageName: "amphitheater.jpg",
+    description:
+      "The amphitheather is an open circular or oval building with a central space surrounded by tiers of seats for spectators, for the presentation of dramatic or sporting events. It is located inside the PUP Lagoon."
+  },
+
+  "freedom-park": {
+    title: "Freedom Park",
+    imageName: "freedom-park.jpg",
+    description:
+      "A public space inside the university where people can hold rallies, demonstrations, gatherings, and flag ceremonies without the administration's permission."
+  },
+
+  "flag-pole": {
+    title: "Flag Pole",
+    imageName: "flag-pole.jpg",
+    description: "The Philippine flag at the center of the campus."
+  },
+
+  ferry: {
+    title: "Sta. Mesa Ferry Station",
+    imageName: "ferry-station.jpg",
+    description:
+      "The Pasig River Ferry Service station inside the campus run by the Metropolitan Manila Development Authority."
+  },
+
+  mural: {
+    title: "Mural Brass Sculture",
+    imageName: "mural.jpg",
+    description:
+      "The sculpture (Cut and welded brass mural relief, 2.5 x 9.3 meters) was built by national artist Eduardo Castrillo † in 1974.  The theme of the artwork is Consolidated Growth through Education - the role of PUP in the educational development of the youth in preparation for their involvement in nation building.  The mural illustrates the social, economic, industrial, technological, and cultural aspect of life with which man blends himself to develop an environment necessary to the progress of the nation."
+  },
+
+  "linear-park": {
+    title: "Linear Park",
+    imageName: "linear-park.jpg",
+    description:
+      "A long linear park near the Pasig River extending from the Laboratory High School to PUP Ferry Station. It is the favorite tambayan of James Reid and Nadine Lustre."
+  },
+
+  gate: {
+    title: "Entrance Gate",
+    imageName: "gate.jpg",
+    description:
+      "The entrance gate of the Polytechnic University of the Philippines."
+  }
+};
+
+function handleCloseModalBtnClick() {
+  const modal = document.getElementById("modal");
+  modal.remove();
+}
+
+function showModal(location) {
+  const modalBackground = document.createElement("div");
+  modalBackground.className = "modal-background";
+  modalBackground.id = "modal";
+
+  const modalCloseBtn = document.createElement("button");
+  modalCloseBtn.innerHTML = "&times;";
+  modalCloseBtn.className = "close";
+  modalCloseBtn.id = "modal-close";
+  modalCloseBtn.onclick = handleCloseModalBtnClick;
+
+  const modalBox = document.createElement("div");
+  modalBox.className = "modal-box";
+
+  const locationImage = document.createElement("img");
+  locationImage.alt = "preview";
+  locationImage.src = "./assets/locations/" + locations[location].imageName;
+
+  const locationName = document.createElement("p");
+  locationName.className = "location-name";
+  locationName.innerText = locations[location].title;
+
+  const locationDescription = document.createElement("p");
+  locationDescription.className = "description";
+  locationDescription.innerText = locations[location].description;
+
+  modalBox.appendChild(modalCloseBtn);
+  modalBox.appendChild(locationImage);
+  modalBox.appendChild(locationName);
+  modalBox.appendChild(locationDescription);
+  modalBackground.appendChild(modalBox);
+
+  document.getElementsByTagName("body")[0].appendChild(modalBackground);
+}
